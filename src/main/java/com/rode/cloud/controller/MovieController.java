61e65c21cafbd9cf9a66b619f6ec4e5ce313f861
roderickzxy <roderickzxy@qq.com>
2017-11-05 21:54:50 +0800

    update the code.

---
+++ src/main/java/com/rode/cloud/controller/MovieController.java
@@ -0,0 +1,24 @@
+package com.rode.cloud.controller;
+
+
+import com.rode.cloud.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+public class MovieController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping("/movie/{id}")
+    public User getUserById(@PathVariable("id") Long id, HttpServletRequest request) {
+        return restTemplate.getForObject("http://localhost:7900/user/" + id, User.class);
+    }
+}
--- src/test/java/com/rode/cloud/MicroserviceSimpleConsumerMovieApplicationTests.java
+++ src/test/java/com/rode/cloud/MicroserviceSimpleConsumerMovieApplicationTests.java
@@ -1,4 +1,4 @@
-package com.rode.cloud.microservicesimpleconsumermovie;
+package com.rode.cloud;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
