da6b0fdb410d11f31c5c237fd721e001cc273067
roderickzxy <roderickzxy@qq.com>
2017-11-08 22:17:26 +0800

    update the pom.xml to suit for other env.

--- src/main/java/com/rode/cloud/controller/MovieController.java
+++ src/main/java/com/rode/cloud/controller/MovieController.java
@@ -4,12 +4,12 @@ package com.rode.cloud.controller;
 import com.rode.cloud.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
-import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import javax.servlet.http.HttpServletRequest;
 
 @RestController
 public class MovieController {
@@ -17,8 +17,8 @@ public class MovieController {
     @Autowired
     private RestTemplate restTemplate;
 
-    @GetMapping("/movie/{id}")
-    public User getUserById(@PathVariable("id") Long id, HttpServletRequest request) {
+    @RequestMapping("/movie/{id}")
+    public User getUserById(@PathVariable("id") Long id) {
         return restTemplate.getForObject("http://localhost:7900/user/" + id, User.class);
     }
 }
